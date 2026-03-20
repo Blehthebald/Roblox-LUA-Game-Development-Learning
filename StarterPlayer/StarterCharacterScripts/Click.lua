16671c4cd14fa158a0135859760e5a1e3158b554
Blehthebald <161674128+Blehthebald@users.noreply.github.com>
2026-03-20 14:42:15 +0800

    Updated StarterPlayer/StarterCharacterScripts/Click.lua

---
+++ StarterPlayer/StarterCharacterScripts/Click.lua
@@ -0,0 +1,14 @@
+-- @ScriptType: LocalScript
+local userinput = game:GetService("UserInputService")
+local replicatedstorage = game:GetService("ReplicatedStorage")
+local clickevent = replicatedstorage:WaitForChild("ClickEvent")
+local justclicked = false
+userinput.InputBegan:Connect(function(input, gameProcessedEvent)
+	if gameProcessedEvent then return end
+	if input.UserInputType == Enum.UserInputType.MouseButton1 and justclicked == false then
+		justclicked = true
+		clickevent:FireServer()
+		wait(0.1)
+		justclicked = false
+	end
+end)

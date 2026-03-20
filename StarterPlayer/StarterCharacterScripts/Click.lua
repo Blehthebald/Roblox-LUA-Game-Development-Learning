da97726a7308dbee0ae1773f1ce12efe0c08ea5a
Blehthebald <161674128+Blehthebald@users.noreply.github.com>
2026-03-20 15:02:27 +0800

    Updated StarterPlayer/StarterCharacterScripts/Click.lua

--- StarterPlayer/StarterCharacterScripts/Click.lua
+++ StarterPlayer/StarterCharacterScripts/Click.lua
@@ -6,9 +6,11 @@ local justclicked = false
 userinput.InputBegan:Connect(function(input, gameProcessedEvent)
 	if gameProcessedEvent then return end
 	if input.UserInputType == Enum.UserInputType.MouseButton1 and justclicked == false then
+		--detects if the user has clicked and fires a click event
 		justclicked = true
 		clickevent:FireServer()
 		wait(0.1)
+		--timer to limit number of clicks per second, otherwise autoclickers would run rampant and destroy the purpose of the game
 		justclicked = false
 	end
 end)

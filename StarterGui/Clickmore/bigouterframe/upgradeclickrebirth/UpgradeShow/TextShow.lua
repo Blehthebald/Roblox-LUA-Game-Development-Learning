b746073f6dc022f6776c743cb0b5903c95381fc4
Blehthebald <161674128+Blehthebald@users.noreply.github.com>
2026-03-20 14:42:04 +0800

    Updated StarterGui/Clickmore/bigouterframe/upgradeclickrebirth/UpgradeShow/TextShow.lua

---
+++ StarterGui/Clickmore/bigouterframe/upgradeclickrebirth/UpgradeShow/TextShow.lua
@@ -0,0 +1,7 @@
+-- @ScriptType: LocalScript
+local text = script.Parent
+local player = game.Players.LocalPlayer
+local rebirth = player:GetAttribute("rebirths")
+
+
+text.Text = rebirth .. "->".. rebirth+1 .. "rebirths"

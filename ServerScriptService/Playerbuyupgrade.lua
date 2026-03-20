f8badc5b8ba60384d3102fee0c10e685f7104881
Blehthebald <161674128+Blehthebald@users.noreply.github.com>
2026-03-20 15:02:26 +0800

    Updated ServerScriptService/Playerbuyupgrade.lua

--- ServerScriptService/Playerbuyupgrade.lua
+++ ServerScriptService/Playerbuyupgrade.lua
@@ -7,6 +7,7 @@ buyevent.OnServerEvent:Connect(function(player)
 	local userupgradelevel = player:GetAttribute("clicknumupgrade")
 	local upgradecost = 2 ^ (userupgradelevel) * 100 * 1.5^(userupgradelevel)
 	local format = require(game.ReplicatedStorage.NumberFormat)
+	--calculates if user has enough currency for the upgrade
 	if usermoney >= upgradecost then
 		player:SetAttribute("Coins", usermoney-upgradecost)
 		player:SetAttribute("clicknumupgrade", userupgradelevel+1)
@@ -15,7 +16,7 @@ buyevent.OnServerEvent:Connect(function(player)
 		price.Text = "Price: ".. format.doit(math.ceil(2^ (userupgradelevel) * 100 * 1.5^(userupgradelevel)))
 		local upgradeshow = player.PlayerGui.Clickmore.bigouterframe.upgradeclicknum.UpgradeShow
 		upgradeshow.Text = format.doit(2 ^ (userupgradelevel)) .. "->" .. format.doit(2 ^ (userupgradelevel+1)) .. "coins per click"
-		
+		--updates values after transaction is complete
 	end
 	
 	

ac6cc5d33252ab556edbfb1c62e4ccaf766f6d8e
Blehthebald <161674128+Blehthebald@users.noreply.github.com>
2026-03-20 15:02:21 +0800

    Updated ServerScriptService/Playerbuyrebirth.lua

--- ServerScriptService/Playerbuyrebirth.lua
+++ ServerScriptService/Playerbuyrebirth.lua
@@ -4,11 +4,15 @@ local buyevent = ReplicatedStorage:WaitForChild("BuyRebirth")
 local function round(num)
 	return math.floor(num * 10 + 0.5) / 10
 end
+--rounds number to 1 dp
+
+--function only triggers when the user buys a rebirth
 buyevent.OnServerEvent:Connect(function(player)
 	local usermoney = player:GetAttribute("Coins")
 	local userupgradelevel = player:GetAttribute("rebirths")
 	local upgradecost = 1000^(userupgradelevel)
 	local format = require(game.ReplicatedStorage.NumberFormat)
+	--calculates if user has enough currency for a rebirth
 	if usermoney >= upgradecost then
 		player:SetAttribute("Coins", usermoney-upgradecost)
 		player:SetAttribute("rebirths", userupgradelevel+1)
@@ -17,13 +21,15 @@ buyevent.OnServerEvent:Connect(function(player)
 		price.Text = "Price: ".. format.doit(math.ceil(1000^(userupgradelevel)))
 		local upgradeshow = player.PlayerGui.Clickmore.bigouterframe.upgradeclickrebirth.UpgradeShow
 		upgradeshow.Text = userupgradelevel .. "->".. userupgradelevel+1 .. "rebirths"
+		--increments rebirths by 1 after the transaction is completed
 		player:SetAttribute("Coins", 0)
 		player:SetAttribute("clicknumupgrade", 0)
 		player:SetAttribute("clickmultiplyupgrade", 0)
 		player.PlayerGui.Clickmore.bigouterframe.upgradeclicknum.Price.Text = "Price: 100"
 		player.PlayerGui.Clickmore.bigouterframe.upgradeclicknum.UpgradeShow.Text = "1 -> 2 coins per click"
 		player.PlayerGui.Clickmore.bigouterframe.upgradeclickmultiply.Price.Text = "Price: 100"
-		player.PlayerGui.Clickmore.bigouterframe.upgradeclickmultiply.UpgradeShow.Text = "1x -> 2x"
+		player.PlayerGui.Clickmore.bigouterframe.upgradeclickmultiply.UpgradeShow.Text = "1x -> 2x coins gain"
+		-- this chunk of code resets the player's upgrades and sets the price and upgrade text to the default values, setting to the default values manually is easier than recalculating it
 	end
 	
 end)

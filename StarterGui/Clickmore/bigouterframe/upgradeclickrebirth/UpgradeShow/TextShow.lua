95182761fb5ff8866600f53860ff6e3f064a8e48
Blehthebald <161674128+Blehthebald@users.noreply.github.com>
2026-03-21 18:56:47 +0800

    Updated StarterGui/Clickmore/bigouterframe/upgradeclickrebirth/UpgradeShow/TextShow.lua

--- StarterGui/Clickmore/bigouterframe/upgradeclickrebirth/UpgradeShow/TextShow.lua
+++ StarterGui/Clickmore/bigouterframe/upgradeclickrebirth/UpgradeShow/TextShow.lua
@@ -2,6 +2,16 @@
 local text = script.Parent
 local player = game.Players.LocalPlayer
 local rebirth = player:GetAttribute("rebirths")
+local replicatedstorage = game:GetService("ReplicatedStorage")
+local dataloaded = replicatedstorage:WaitForChild("DataLoaded")
 
+local co2 = coroutine.create(function()
 
-text.Text = rebirth .. "->".. rebirth+1 .. "rebirths"
+	text.Text = rebirth .. "->".. rebirth+1 .. "rebirths"
+
+end)
+print("hi")
+coroutine.resume(co2)
+dataloaded.OnClientEvent:Connect(function()
+	coroutine.resume(co2)
+end)
